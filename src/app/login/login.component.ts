ced490516e0b1200083abdfaefe4a7f75fd51687
osmanBAyn <osbaran10@gmail.com>
2024-06-03 13:33:03 +0300

    Login page frontend and reactive form

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  constructor(private fb : FormBuilder) {}
+  loginForm = this.fb.group({
+    email: [''],
+    password: ['']
+  });
+}
